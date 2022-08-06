d4a62f83dd9101b48dc0c1264c245bd214ae9207
TeoVincent <artur.wincenciak@gmail.com>
2022-08-07 00:05:56 +0200

    xUnit test proj

---
+++ AzureDevOpsGettingStarted.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AzureDevOpsGettingStarted.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true, "Always green unit test.");
+        }
+    }
+}
