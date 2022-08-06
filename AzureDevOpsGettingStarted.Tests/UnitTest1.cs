e94b2a24df062d71c75970def55a28a3ad17e4e0
TeoVincent <artur.wincenciak@gmail.com>
2022-08-07 00:19:54 +0200

    Always red unit test

--- AzureDevOpsGettingStarted.Tests/UnitTest1.cs
+++ AzureDevOpsGettingStarted.Tests/UnitTest1.cs
@@ -3,9 +3,15 @@ namespace AzureDevOpsGettingStarted.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Green()
         {
             Assert.True(true, "Always green unit test.");
+        }
+
+        [Fact]
+        public void Red()
+        {
+            Assert.True(false, "Always red unit test.");
         }
     }
 }
