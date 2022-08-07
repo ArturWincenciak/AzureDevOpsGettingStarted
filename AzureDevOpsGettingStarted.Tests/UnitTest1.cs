d2109e9f8b9a67121b78187f9531af6a3ef06125
TeoVincent <artur.wincenciak@gmail.com>
2022-08-07 02:12:15 +0200

    Revert "Mark red test to run"
    This reverts commit 79fc582869f07fb49c0a071f1066d58464285f58.

--- AzureDevOpsGettingStarted.Tests/UnitTest1.cs
+++ AzureDevOpsGettingStarted.Tests/UnitTest1.cs
@@ -10,7 +10,6 @@ namespace AzureDevOpsGettingStarted.Tests
 
 
         // [Fact(Skip = "Skip always red unit test.")]
-        [Fact]
         public void Red()
         {
             Assert.True(false, "Always red unit test.");
