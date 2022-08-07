751d251c73e15d663f85ef63c169a26c07e845c5
TeoVincent <artur.wincenciak@gmail.com>
2022-08-07 02:58:24 +0200

    Format code

--- AzureDevOpsGettingStarted.Tests/UnitTest1.cs
+++ AzureDevOpsGettingStarted.Tests/UnitTest1.cs
@@ -1,17 +1,16 @@
-namespace AzureDevOpsGettingStarted.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Green()
-        {
-            Assert.True(true, "Always green unit test.");
-        }
+namespace AzureDevOpsGettingStarted.Tests;
 
-        [Fact(Skip = "Skip always red unit test.")]
-        public void Red()
-        {
-            Assert.True(false, "Always red unit test.");
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Green()
+    {
+        Assert.True(true, "Always green unit test.");
+    }
+
+    [Fact(Skip = "Skip always red unit test.")]
+    public void Red()
+    {
+        Assert.True(false, "Always red unit test.");
     }
 }
